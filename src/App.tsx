3e16471b1184b4f5a6812e3b54748f3066116492
Jaskaran Singh <bestfxon@gmail.com>
2022-07-12 23:23:01 -0500

    preview added
    Co-authored-by: Ashfahan <work@ashfahan.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import { Canvas } from "./Canvas"
+import { AppCanvas } from "./Canvas"
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header> */}
 
-      <Canvas />
+      <AppCanvas />
     </div>
   )
 }
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,57 +1,26 @@
-import { fabric } from "fabric"
-import { useRef, useEffect } from "react"
-import sample from "./sample.json"
 import "./Canvas.css"
+import { Canvas } from "react-design-editor"
+import sample from "./sample.json"
+import React from "react"
 
-export const Canvas = () => {
-  const canvasEl = useRef(null)
-
-  useEffect(() => {
-    const workarea = sample.objects.find((obj) => obj.id === "workarea")
-    console.log(workarea)
-
-    const mergedCanvasOption = Object.assign(
-      {},
-      {
-        preserveObjectStacking: true,
-        selection: true,
-        defaultCursor: "default",
-        width: (workarea?.workareaWidth ?? 1) * (workarea?.scaleX ?? 1) || 0,
-        height: (workarea?.workareaHeight ?? 1) * (workarea?.scaleY ?? 1) || 0,
-      }
-    )
-    const canvas = new fabric.Canvas(canvasEl.current, mergedCanvasOption)
-
-    const objects = sample.objects.filter((obj) =>
-      obj.id !== "workarea" ? { ...obj, width: obj.width * obj.scaleX, height: obj.height * obj.scaleY } : false
-    )
-
-    const mergeToJson = JSON.parse(JSON.stringify(sample))
-    mergeToJson.objects = objects
-
-    canvas.loadFromJSON(mergeToJson, () => {
-      canvas.renderAll()
-    })
-
-    canvas.setBackgroundColor("red", canvas.renderAll.bind(canvas))
-
-    // if (workarea) {
-    //   const image = new Image(workarea.width, workarea.height)
-    //   const workareaObject = Object.fromEntries(
-    //     Object.entries(workarea).map(([key, value]) => [key, value === null ? undefined : value])
-    //   )
-    //   const workareaObj = new fabric.Image(image, workareaObject)
-    //   canvas.add(workareaObj)
-    //   //   canvas.centerObject(workareaObj)
-    //   //   canvas.setActiveObject(workareaObj)
-    // }
-    // canvas.loadFromJSON(sample, canvas.renderAll.bind(canvas))
-    // canvas.renderAll()
-  }, [])
+export const AppCanvas = () => {
+  const objects = sample.objects.map(object => {
+    if (object.id === "workarea") return { ...object, backgroundColor: "rgba(0,0,0,0)" }
+    return object
+  })
 
   return (
-    // <div className="preview-container">
-    <canvas ref={canvasEl} />
-    // </div>
+    <Canvas
+      editable={false}
+      className="rde-canvas"
+      canvasOption={{
+        perPixelTargetFind: true,
+        backgroundColor: "rgba(0,0,0,0)"
+      }}
+      keyEvent={{
+        grab: false
+      }}
+      onLoad={handler => handler.importJSON(objects)}
+    />
   )
 }
